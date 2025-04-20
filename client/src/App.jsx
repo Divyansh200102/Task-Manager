226ac0b70ab1fc421480cc153be83fe21f7f5173
Divyansh200102 <146909065+Divyansh200102@users.noreply.github.com>
2025-04-21 01:05:41 +0530

    project initlisation

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+
+const App = () => {
+  return (
+    <MantineProvider>
+    <div>
+      App
+      </div>
+      
+    </MantineProvider>
+  )
+}
+
+export default App
